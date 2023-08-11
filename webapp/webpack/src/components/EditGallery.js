9f0f19032feaa97ed6780951f1db32ad09ca9a98
James Andrew Croney <techtruth@desktop.highcotton.jamescroney.com>
2023-08-11 00:33:36 -0400

    save data works

--- webapp/webpack/src/components/EditGallery.js
+++ webapp/webpack/src/components/EditGallery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getGalleryEntries } from "../database/gallery";
 import { addGalleryEntry, removeGalleryEntry } from "../database/galleryEntry";
 //import { addGalleryEntryAttribute, removeGalleryEntryAttribute} from "../database/galleryEntryAttribute";
@@ -15,34 +15,74 @@ export default class editGallery extends React.Component {
     }
   }
 
+  handleNameChange = (index, newName) => {
+    const updatedEntries = [...this.state.entries];
+    updatedEntries[index].name = newName;
+    this.setState({ entries: updatedEntries });
+  };
+
+  handleArtistChange = (index, newArtist) => {
+    const updatedEntries = [...this.state.entries];
+    updatedEntries[index].artist = newArtist;
+    this.setState({ entries: updatedEntries });
+  };
+
+  handleAppraisalChange = (index, newAppraisal) => {
+    const updatedEntries = [...this.state.entries];
+    updatedEntries[index].appraisal = newAppraisal;
+    this.setState({ entries: updatedEntries });
+  };
+
+  saveEntry = (index) => {
+    // Here you can save the updated entries to your backend or perform any other necessary actions.
+    console.log("Saving entries:", this.state.entries[index]);
+  };
+
+  deleteEntry = (index) => {
+    const updatedEntries = this.state.entries.filter((_, i) => i !== index);
+    this.setState({ entries: updatedEntries });
+  };
+
+
   async componentDidMount() {
     let entries = await getGalleryEntries(this.state.galleryName)
     this.setState({ entries });
   }
 
-  Gallery(props) {
-      return <div>
-               { props.entries.map( (entry, index) => (
-                 <ul key={ index }>
-                   <li>Name: { entry.name }</li>
-                   <li>Artist: { entry.artist }</li>
-                   <li>Appraisal: { entry.appraisal }</li>
-                   <li>Image: <img src={ entry.imageData }/></li>
-                 </ul>    
+  Gallery = (props) => {
+      const { entries } = props;
+      return ( <div className="gallery">
+               { entries.map( (entry, index) => (
+                 <div key={ index } className="galleryEntry">
+                   <img src={ entry.imageData } />
+                   <div className="overlay">
+                     <div className="text">
+                       <span>Name: <input value={ entry.name } 
+                                          onChange={(e) => this.handleNameChange(index, e.target.value)} /></span>
+                       <span>Artist: <input value={ entry.artist } 
+                                          onChange={(e) => this.handleArtistChange(index, e.target.value)} /></span>
+                       <span>Appraisal: <input value={ entry.appraisal } 
+                                          onChange={(e) => this.handleAppraisalChange(index, e.target.value)} /></span>
+                     </div>
+                     <div className="buttons">
+                       <button onClick={() => this.saveEntry(index)} >Save</button>
+                       <button onClick={() => this.deleteEntry(index)} >Delete</button>
+                     </div>
+                   </div>
+                 </div>
                ) ) }
-             </div> 
+             </div> ) 
   }
 
   render() {
     const { galleryName, entries } = this.state;
-    //console.log("RENDERING", this.state, galleryName, entries, entries.length);
     if (entries.length) {
       return <div> 
-             <h2>Edit Gallery Page</h2>
+             <h2>Edit { galleryName } Gallery Page</h2>
              <this.Gallery entries={entries}/>
              </div>
     } else {
-      return <div><h2>No Entries in this gallery!</h2></div>
+      return <div><h2>No Entries in { galleryName } this gallery!</h2></div>
     }
 
   }
--- webapp/webpack/src/index.js
+++ webapp/webpack/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./style.css";
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
